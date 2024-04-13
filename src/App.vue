b7cf3a5a45b1b9e69f2203c9e9945972394a07bd
Billy Charlton <billy@okbecause.com>
2024-04-13 21:53:20 +0200

    table shows up

--- src/App.vue
+++ src/App.vue
@@ -7,28 +7,77 @@ const DB_PATH = '/tmip.db.zst';
 
 import { defineComponent } from 'vue';
 import { ZSTDDecoder } from 'zstddec';
-import initSQL from 'sql.js';
-
+import initSQL, { Database } from 'sql.js';
 import sqlWasm from 'sql.js/dist/sql-wasm.wasm?url';
-
-console.log({ initSQL });
+import debounce from 'debounce';
 
 export default defineComponent({
   data() {
     return {
-      count: 0,
-      db: null as null | Buffer,
+      results: [] as any[],
+      db: null as null | Database,
+      searchTerm: '',
+      debounceQuery: {} as any,
     };
   },
 
   async mounted() {
-    // this happens async
+    this.debounceQuery = debounce(this.query, 333);
+
     const buffer = await this.fetchSqliteDatabase();
-    this.initializeDatabaseFromBuffer(buffer);
+    this.db = await this.initializeDatabaseFromBuffer(buffer);
+
+    //tables
+    const query = this.db.exec("SELECT name FROM sqlite_master where type='table';");
+    const tables = query[0].values.flat();
+    console.log({ tables });
+
+    // test
+    this.query('messages');
+    // const messages = this.query('messages');
+    // console.log({ messages });
+    // this.results = messages.slice(0, 100);
+  },
+
+  watch: {
+    searchTerm() {
+      this.debounceQuery('messages', { search: this.searchTerm });
+    },
   },
 
   methods: {
-    async initializeDatabaseFromBuffer(buffer: any) {
+    query(table: string, options?: any) {
+      if (!this.db) return [];
+
+      let query = `SELECT * FROM ${table}`;
+
+      if (options?.search && options.search !== '') {
+        query = `SELECT * FROM ${table} WHERE from_field LIKE '%${options.search}%'`;
+      }
+
+      query += ';';
+
+      console.log(query);
+      const response = this.db.exec(query);
+      const columns = response[0].columns as string[];
+      const answer = response[0].values.map((row: any[]) => {
+        const obj = {} as any;
+        columns.forEach((col, i) => (obj[col] = row[i]));
+        // scrub email addresses
+        if (obj.from_field) {
+          let email = obj.from_field as string;
+          let lt = email.indexOf('<');
+          if (lt > -1) email = email.substring(0, lt);
+          email = email.replaceAll('"', '').trim();
+          obj.from_field = email;
+        }
+        return obj;
+      });
+
+      this.results = answer.slice(0, 100);
+    },
+
+    async initializeDatabaseFromBuffer(buffer: Uint8Array) {
       const SQL = await initSQL({
         // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
         // You can omit locateFile completely when running in node
@@ -36,10 +85,7 @@ export default defineComponent({
       });
 
       const LOADED_DB = new SQL.Database(buffer);
-      console.log({ LOADED_DB });
-
-      const response = LOADED_DB.exec('SELECT * FROM webinars;');
-      console.log({ response });
+      return LOADED_DB;
     },
 
     async fetchSqliteDatabase() {
@@ -49,13 +95,8 @@ export default defineComponent({
 
       // fetch & uncompress archive
       const archive = await (await fetch(DB_PATH)).arrayBuffer();
-      console.log({ archive });
       const buffer = decoder.decode(new Uint8Array(archive));
-      console.log('BUFFER bytes', buffer.byteLength);
-
-      this.count++;
-
-      // Set up SQLite database from buffer
+      console.log('DB BUFFER bytes', buffer.byteLength);
 
       return buffer;
     },
@@ -64,5 +105,23 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>count is {{ count }}</div>
+  <div class="top-bar">
+    <input
+      class="input is-link"
+      style="flex: 1"
+      type="text"
+      placeholder="Search..."
+      v-model="searchTerm"
+    />
+    <button class="button is-link">Search</button>
+  </div>
+  <div class="results-table">
+    <div v-for="row in results" class="results-row">
+      <div class="row-date">{{ row.date.split(' ')[0] }}</div>
+      <div>
+        <div class="row-subject">{{ row.subject }}</div>
+        <div class="row-from">{{ row.from_field }}</div>
+      </div>
+    </div>
+  </div>
 </template>
