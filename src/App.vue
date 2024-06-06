c2e2f01a54227dd0bffdd016cc072c76309d5da8
Billy Charlton <mail@billyc.cc>
2024-06-06 10:08:22 +0200

    scope css only for tmip

--- src/App.vue
+++ src/App.vue
@@ -258,8 +258,8 @@ export default defineComponent({
 })
 </script>
 
-<style>
-@import './css/zephyr-2020.css';
-@import './css/landing-page.css';
+<style scoped>
+/* @import './css/zephyr-2020.css'; */
+/* @import './css/landing-page.css'; */
 @import './css/style.css';
 </style>
